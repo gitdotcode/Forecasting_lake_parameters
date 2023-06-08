ecb19b2eb00f9030f48b15348fb2cfef9217a2c0
gitdotcode <110038436+gitdotcode@users.noreply.github.com>
2023-06-08 11:39:04 +0530

    Update appupdated.py

--- appupdated.py
+++ appupdated.py
@@ -7,7 +7,7 @@ import streamlit as st
 import pickle
 from pickle import dump
 from pickle import load
-import statsmodels
+
 
 from statsmodels.tsa.seasonal import seasonal_decompose
 from statsmodels.tsa.holtwinters import SimpleExpSmoothing
